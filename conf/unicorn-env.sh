a0e43486bfdef3b8c0cb4093baf35261eee2b4ca
XavierQA <xavierqa@gmail.com>
2014-08-28 16:43:01 -0700

    adding configuration files

---
+++ conf/unicorn-env.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#environment varialbles
+
+#### SET Java Home
+if [ -z $JAVA_HOME ]; then
+        export JAVA_HOME=/usr/lib/jvm/default-java
+
+fi
+
+### SET Nutch Home
+if [ -z $NUTCH_HOME ]; then
+        NUTCH_HOME=/home/xavier/nutchTest
+        if [ ! -d $NUTCH_HOME ]; then
+                mkdir $NUTCH_HOME
+        fi
+
+fi
+
+
+if [ -z $UNICORN_HOME ]; then
+        UNICORN_HOME=/home/xavier/workspace/Hadoop_TFIDF
+fi
+
+
+UNICORN_HOME=$UNICORN_HOME
+
+OUTPUTURL=unicorn_url_n
+OUTPUTTF=unicorn_tf_n
+OUTPUTIDF=unicorn_idf_n
+OUTPUTTFIDF=unicorn_tfidf_n
+OUTPUTVECTOR=unicorn_vector
+
+#NUTCH HOME
+
+URLSEED=$NUTCH_HOME/nutch/runtime/local/urls
+CRAWLEROUTPUT=$NUTCH_HOME/nutch/runtime/local/output
+DEEP=1
+NUTCHHOME=$NUTCH_HOME/nutch/runtime/local
+
+
+BASEDIR=$CRAWLEROUTPUT
+CRAWLDB=${BASEDIR}/crawldb/current
+LINKDB=${BASEDIR}/linkdb/current
+SEGMENTSDIR=${BASEDIR}/segments/
+#OUTPUTPATH="-output ${OUTPUT}"
+#OUTPUTTF="-output ${OUTTF}"
+
